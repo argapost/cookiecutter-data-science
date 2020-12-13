117823553e80a1dc2e8c228a1b55cc085ec42ab1
Argyris <arapo4@gmail.com>
2020-12-13 13:05:43 +0100

    correct setup package

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -2,7 +2,6 @@ from setuptools import find_packages, setup
 
 setup(
     name='{{ cookiecutter.project_name }}',
-    packages=find_packages(),
     version='0.1.0',
     description='{{ cookiecutter.description }}',
     author='{{ cookiecutter.author_name }}',
