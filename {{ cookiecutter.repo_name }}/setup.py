3862834f71dedb4c8d43709c733d2d3ad8d87dc8
Argyris <arapo4@gmail.com>
2020-12-13 19:21:52 +0100

    change structure to follow spectralDNS

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -8,4 +8,10 @@ setup(
     license='{% if cookiecutter.open_source_license == 'MIT' %}MIT{% elif cookiecutter.open_source_license == 'BSD-3-Clause' %}BSD-3{% endif %}',
     packages=find_packages(''),
     package_dir={"": "src"},
+    packages=['{{ cookiecutter.project_name }}',
+        '{{ cookiecutter.project_name }}.features',
+        '{{ cookiecutter.project_name }}.visualization',
+        '{{ cookiecutter.project_name }}.data',
+        ],
+    package_dir={'{{ cookiecutter.project_name }}': '{{ cookiecutter.project_name }}'},
 )
